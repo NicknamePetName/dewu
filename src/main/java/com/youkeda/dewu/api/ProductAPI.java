e998a56ab4749fd2220016f71bd97f377098d628
Nickname <1013238728@qq.com>
2024-04-12 23:11:30 +0800

    Nickname

--- src/main/java/com/youkeda/dewu/api/ProductAPI.java
+++ src/main/java/com/youkeda/dewu/api/ProductAPI.java
@@ -9,16 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/api/product")
 public class ProductAPI {
 
     @Autowired
     private ProductService productService;
 
     @ResponseBody
-    @GetMapping("/api/product/page")
+    @GetMapping("/page")
     public Result<Paging<Product>> queryPage(@RequestBody BasePageParam param) {
         Result<Paging<Product>> result = new Result<>();
         result.setData(productService.pageQueryProduct(param));
